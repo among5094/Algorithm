5633b9c968b24e997c3025f215acaa6ceef6247e
among5094 <among5094@gmail.com>
2022-12-26 21:41:13 +0900

    배열 두 배 만들기(성공)

--- programmes/Lv0/배열_두배_만들기.c
+++ programmes/Lv0/배열_두배_만들기.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
+//문제: 배열 두배 만들기 
+//문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/120809
 // numbers_len은 배열 numbers의 길이입니다.
 int* solution(int numbers[], size_t numbers_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
