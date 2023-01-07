01619c79ce8630a66d908eb06d570d9ae340abc7
among5094 <among5094@gmail.com>
2023-01-07 18:12:58 +0900

    성공

--- programmes/Lv0/순서쌍의_개수.c
+++ programmes/Lv0/순서쌍의_개수.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-//문제: 순서쌍의 개수 return하기
+//문제: 순서쌍의 개수 return하기, 약수 찾기
 //문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/120836
 
 int solution(int n)
@@ -11,9 +11,21 @@ int solution(int n)
     
     for(int i=1; i<=n; i++)
     {
-        if(i*(n-1-i) ==20)
+        if(i*(n-1-i) ==n )
             answer++;
         
     }
     return answer;
 }
+
+//다른 사람 풀이
+/*
+for(int i=1; i<=n; i++)
+    {
+        if( i%n==0 )
+            answer++;
+        
+    }
+
+    return 0;
+*/
