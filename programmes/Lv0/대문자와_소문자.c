74ec4dd8b9f3725c7d994eac1f2573f2783d4b44
among5094 <among5094@gmail.com>
2023-02-01 17:41:31 +0900

    대문자와 소문자 성공

--- programmes/Lv0/대문자와_소문자.c
+++ programmes/Lv0/대문자와_소문자.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-char* solution(const char* my_string) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+//제목: 대문자와 소문자
+//문제: 대문자는 소문자로, 소문자는 대문자로 변환해서 return 하기
+//문제 출처: https://school.programmers.co.kr/learn/courses/30/lessons/120893
+
+char* solution(const char* my_string)
+{
+    int len = strlen(my_string); //문자열 my_string의 개수
     char* answer = (char*)malloc(1);
     return answer;
 }
+
+/*
+toupper(c) : 문자c를 대문자로 바꾼다. 
+tolower(c) : 문자c를 소문자로 바꾼다. 
+
+strlwr()
+
+*/
